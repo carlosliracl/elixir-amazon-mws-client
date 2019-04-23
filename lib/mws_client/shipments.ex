fb3bae75287770cb70cc4294ac1b19da8156ecc6
Carlos Lira <carlos.reis@skyhub.com.br>
2019-04-23 11:31:40 -0300

    add shipments module

---
+++ lib/mws_client/shipments.ex
@@ -0,0 +1,33 @@
+defmodule MWSClient.Shipments do
+  @version "2018-09-01"
+  @path "/ShipmentInvoicing/#{@version}"
+
+  @moduledoc """
+  Fulfillment Outbound Shipment API section of Amazon MWS
+  """
+
+  import MWSClient.Utils
+
+  def get_fba_outbound_shipment_detail(params, opts) do
+    %{"Action" => "GetFBAOutboundShipmentDetail"}
+    |> add(params, [:amazon_shipment_id])
+    |> add(opts, [:marketplace_id])
+    |> to_operation(@version, @path)
+  end
+
+  def submit_fba_outbound_shipment_invoice(params, opts) do
+    data = Keyword.get(params, :data)
+
+    %{"Action" => "SubmitFBAOutboundShipmentInvoice", "ContentMD5Value" => content_md5(data)}
+    |> add(params, [:amazon_shipment_id, :content_md5_value])
+    |> add(opts, [:marketplace_id])
+    |> to_operation(@version, @path, data)
+  end
+
+  def get_fba_outbound_shipment_invoice_status(params, opts) do
+    %{"Action" => "GetFBAOutboundShipmentInvoiceStatus"}
+    |> add(params, [:amazon_shipment_id])
+    |> add(opts, [:marketplace_id])
+    |> to_operation(@version, @path)
+  end
+end
---
+++ test/mws_client/shipments_test.exs
@@ -0,0 +1,68 @@
+defmodule MWSClient.ShipmentsTest do
+  use ExUnit.Case
+
+  alias MWSClient.Shipments
+  alias MWSClient.Operation
+
+  test "get_fba_outbound_shipment_detail" do
+    result =
+      Shipments.get_fba_outbound_shipment_detail(
+        [amazon_shipment_id: "shipment_id"],
+        marketplace_id: "ATVPDKIKX0DER"
+      )
+
+    expectation = %Operation{
+      params: %{
+        "AmazonShipmentId" => "shipment_id",
+        "Action" => "GetFBAOutboundShipmentDetail",
+        "MarketplaceId" => "ATVPDKIKX0DER",
+        "Version" => "2018-09-01"
+      },
+      path: "/ShipmentInvoicing/2018-09-01"
+    }
+
+    assert result == expectation
+  end
+
+  test "submit_fba_outbound_shipment_invoice" do
+    result =
+      Shipments.submit_fba_outbound_shipment_invoice(
+        [data: "xml content file", amazon_shipment_id: "shipment_id"],
+        marketplace_id: "ATVPDKIKX0DER"
+      )
+
+    expectation = %Operation{
+      body: "xml content file",
+      params: %{
+        "AmazonShipmentId" => "shipment_id",
+        "Action" => "SubmitFBAOutboundShipmentInvoice",
+        "MarketplaceId" => "ATVPDKIKX0DER",
+        "Version" => "2018-09-01",
+        "ContentMD5Value" => "KoXUCoEHwYTCPipqsKslTw=="
+      },
+      path: "/ShipmentInvoicing/2018-09-01"
+    }
+
+    assert result == expectation
+  end
+
+  test "get_fba_outbound_shipment_invoice_status" do
+    result =
+      Shipments.get_fba_outbound_shipment_invoice_status(
+        [amazon_shipment_id: "shipment_id"],
+        marketplace_id: "ATVPDKIKX0DER"
+      )
+
+    expectation = %Operation{
+      params: %{
+        "AmazonShipmentId" => "shipment_id",
+        "Action" => "GetFBAOutboundShipmentInvoiceStatus",
+        "MarketplaceId" => "ATVPDKIKX0DER",
+        "Version" => "2018-09-01"
+      },
+      path: "/ShipmentInvoicing/2018-09-01"
+    }
+
+    assert result == expectation
+  end
+end
